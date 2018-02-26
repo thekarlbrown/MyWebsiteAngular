cca7ef3c762759ef36e79a33f03849f6cc4360b4
Karl Brown <karl@thekarlbrown.com>
2018-02-26 18:44:04 -0500

    Added bootstrap css, changed favicon, added barebones menu bar

--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule
